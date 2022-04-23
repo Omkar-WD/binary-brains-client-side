ea178cf4824fc3ee6c3f47063e7c062f6a7448f0
Nikhil874 <95832751+Nikhil874@users.noreply.github.com>
2022-04-23 21:24:56 +0530

    value-updated

--- src/components/Admin/Assignments.jsx
+++ src/components/Admin/Assignments.jsx
@@ -17,27 +17,35 @@ export const AdminAssignments = () => {
   }
   const handleSubmit=(e)=>{
     axios.post("https://binary-brains.herokuapp.com/assignment",data).then((res)=>{
+      setData({
+        assignment_name:"",
+        batch_name:"",
+        type:"",
+        creater:"",
+        created_date:"",
+        dead_line:""
+      })
       console.log(res);
     })
   }
   return <><h1>Create Assignment</h1> 
    <Input placeholder="assignment name" width={550} id="assignment_name" onChange={(e)=>{handleChange(e)}} 
-   />
+   value={data.assignment_name}/>
    <br />
    <br />
-   <Input placeholder="batch name" width={550} id="batch_name" onChange={(e)=>{handleChange(e)}}/>
+   <Input placeholder="batch name" width={550} id="batch_name" onChange={(e)=>{handleChange(e)}} value={data.batch_name}/>
    <br />
    <br />
-   <Input placeholder="type" width={550} id="type" onChange={(e)=>{handleChange(e)}}/>
+   <Input placeholder="type" width={550} id="type" onChange={(e)=>{handleChange(e)}} value={data.type}/>
    <br />
    <br />
-   <Input placeholder="Creator" width={550} id="creater" onChange={(e)=>{handleChange(e)}}/>
+   <Input placeholder="Creator" width={550} id="creater" onChange={(e)=>{handleChange(e)}} value={data.creater}/>
    <br />
    <br />
-   <Input placeholder="created Date" type="date" width={550} id="created_date"  onChange={(e)=>{handleChange(e)}}/>
+   <Input placeholder="created Date" type="date" width={550} id="created_date"  onChange={(e)=>{handleChange(e)}} value={data.created_date}/>
    <br />
    <br />
-   <Input placeholder="Dead line" type="date" width={550} id="dead_line" onChange={(e)=>{handleChange(e)}}/>
+   <Input placeholder="Dead line" type="date" width={550} id="dead_line" onChange={(e)=>{handleChange(e)}} value={data.dead_line}/>
    <br />
    <br />
    <Button colorScheme='teal' size='sm' onClick={handleSubmit}>
